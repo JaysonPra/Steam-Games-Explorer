85d17db1678e77704304a4397108048653a820dd
Jayson Pradhananga <jaysonyt2@gmail.com>
2025-08-16 19:00:01 +0545

    Game Explorer page

--- streamlit-app/Pages/game_explorer.py
+++ streamlit-app/Pages/game_explorer.py
@@ -1,9 +1,24 @@
 import streamlit as st
 import pandas as pd
+import math
 
 from utils import load_and_clean_data
+from views.explorer import game_explorer
 
-st.set_page_config(page_title="Steam Explorer - Game Explorer")
+st.set_page_config(page_title="Steam Explorer - Game Explorer", layout="wide")
+
+st.markdown(
+    """
+    <style>
+    .stMainBlockContainer {
+        padding-top: 1.5rem; 
+        padding-bottom: 1rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 steam_games = load_and_clean_data()
 
+game_explorer(steam_games)
